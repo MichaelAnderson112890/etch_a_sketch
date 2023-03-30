1b765625850d67505903ca721e7ca9676118c44a
Michael Anderson <Michael.anderson112890@gmail.com>
2023-03-30 12:06:34 -0700

    Hover over sketch pad has been done. Created buttons for reset and changing the amout of squares to use.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,55 @@
-function createSquare() {
+let size = 10;
+let mouseDown = 0;
 
-    const board = document.querySelector("#board");
-    for (let i = 0; i < 4; i++){
+document.body.onmousedown = function () {++mouseDown;}
+document.body.onmouseup = function () {--mouseDown;}
+function createSquare(size) {
+    const board = document.querySelector("#sketchpad");
+    for (let i = 0; i < size; i++){
         const column = document.createElement('div');
         column.setAttribute('class','column');
-        for (let j = 0; j < 4; j++) {
+        for (let j = 0; j < size; j++) {
             const row = document.createElement('button');
             row.setAttribute('class','row');
             row.classList.add('content')
+            row.addEventListener('mouseover', () => {
+                if (mouseDown === 1){
+                    row.style.backgroundColor = 'black';
+                }
+            });
+
             column.append(row);
         }
         board.append(column);
     }
+}
 
-
+function reset(size) {
+    const sketchPad = document.querySelector('#sketchpad');
+    sketchPad.remove();
+    const newSketchPad = document.createElement('div');
+    newSketchPad.setAttribute('id','sketchpad');
+    const board = document.querySelector('#board');
+    board.append(newSketchPad);
+    createSquare(size);
 
 }
 
-createSquare();
+function changeSize() {
+    newSize = prompt('What size would you like the sketch pad to be?');
+    console.log(newSize);
+    size = newSize;
+    reset(size)
+}
+createSquare(size);
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => {
+    reset(size);
+});
+const sizeButton = document.getElementById('size');
+sizeButton.addEventListener('click', () => {
+    changeSize();
+});
 
 
 /*
